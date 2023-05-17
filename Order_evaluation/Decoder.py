69fbdb6202cb348c05f588d69bc05a636d1a2a2f
AleksiusK <kurkela.aleksius@gmail.com>
2023-05-17 12:59:47 +0300

    Created a transformer model using Autoformer's decoder layer to process the created forecast without the order's effects.

---
+++ Order_evaluation/Decoder.py
@@ -0,0 +1,67 @@
+import torch.nn as nn
+from Order_evaluation.autocorrelation_layer import autocorrelation_layer
+from Order_evaluation.series_decomposition import series_decomposition_block
+from Order_evaluation.feed_forward import FeedForward
+from Order_evaluation.Time2VecEmbed import Embed
+from Order_evaluation.MLP import MLP
+
+
+class autoformer_decoder(nn.Module):
+    def __init__(self, c: int, window: int, prediction_len: int, num_layers: int,
+                 num_heads: int, trainable: bool, d_model: int, decoders: int):
+        super(autoformer_decoder, self).__init__()
+        self.c = c
+        self.window = window
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.trainable = trainable
+        self.prediction_len = prediction_len
+        self.num_layers = num_layers
+        self.decoders = decoders
+
+        self.autocorrelation1 = autocorrelation_layer(c=self.c,
+                                                      output=self.prediction_len, heads=self.num_heads,
+                                                      trainable=self.trainable, d_model=self.d_model)
+
+        self.autocorrelation2 = autocorrelation_layer( c=self.c,
+                                                      output=self.prediction_len, heads=self.num_heads,
+                                                      trainable=self.trainable, d_model=self.d_model)
+
+        self.series_decomp1 = series_decomposition_block(window=int(self.window / 2 + self.prediction_len),
+                                                         num_heads=self.d_model, return_trend=True)
+        self.series_decomp2 = series_decomposition_block(window=int(self.window / 2 + self.prediction_len),
+                                                         num_heads=self.d_model, return_trend=True)
+        self.series_decomp3 = series_decomposition_block(window=int(self.window / 2 + self.prediction_len),
+                                                         num_heads=self.d_model, return_trend=True)
+        self.feed_forward = FeedForward(d_model=self.d_model, dropout=0.1, dim_ff=self.d_model * 4)
+        self.MLP = nn.ModuleList([MLP(d_model=self.d_model, out_length=6, num_layers=self.num_layers) for
+                                  _ in range(4)])
+        self.embed = Embed(self.d_model, self.window)
+
+    def forward(self, x_de, x_en, x_mean):
+        x_de = self.embed(x_de)
+
+        for _ in range(self.decoders):
+            x_des = self.autocorrelation1(x_de)
+            x_des += x_de
+
+            s_de, t_de = self.series_decomp1(x_des)
+
+            x_des_2 = self.autocorrelation2((x_de, x_en))
+            x_des = x_des_2 + s_de
+
+            s_de, t_de_1 = self.series_decomp2(x_des)
+
+            x_des = self.feed_forward(s_de)
+            x_des += s_de
+
+            x_de, t_de_2 = self.series_decomp3(x_des)
+
+            t_de = self.MLP[0](t_de)
+            t_de_1 = self.MLP[1](t_de_1)
+            t_de_2 = self.MLP[2](t_de_2)
+            x_mean = x_mean + t_de + t_de_1 + t_de_2
+
+        x_de = self.MLP[3](x_de)
+
+        return x_de, x_mean
---
+++ Order_evaluation/Order_eval.py
@@ -0,0 +1,81 @@
+import logging
+
+import torch
+import torch.nn as nn
+
+from Order_evaluation.Decoder import autoformer_decoder
+from Order_evaluation.OE_encoder import OE_Encoder
+from Order_evaluation.series_decomposition import series_decomposition_block
+
+logging.basicConfig(filename='training.log', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
+
+class order_evaluation(nn.Module):
+    def __init__(self, window: int, c: int, pred_len: int, num_layers: int, num_heads: int,
+                 trainable: bool, d_model: int, encoders: int, decoders: int, order_dim: int):
+        super(order_evaluation, self).__init__()
+        self.num_heads = num_heads
+        self.trainable = trainable
+        self.c = c
+        self.order_dim = order_dim
+        self.pred_len = pred_len
+        self.d_model = d_model
+        self.out_len = int(window / 2) + self.pred_len
+        self.num_layers = num_layers
+        self.window = window
+        self.encoders = encoders
+        self.decoders = decoders
+
+        self.encoder = OE_Encoder(order_dim=self.order_dim, dim_ff=self.d_model * 4, d_model=self.d_model, dropout=0.1,
+                                  eps=1e-9, n_heads=num_heads, n_encoders=self.encoders)
+
+        self.decoder = autoformer_decoder(c=self.c, window=self.window,
+                                          prediction_len=self.pred_len, num_layers=self.num_layers,
+                                          trainable=self.trainable, num_heads=self.num_heads, d_model=self.d_model,
+                                          decoders=self.decoders)
+        self.series_decomp = series_decomposition_block(window=int(self.window / 2), num_heads=self.num_heads,
+                                                        return_trend=True)
+
+    def forward(self, inputs, *args, **kwargs):
+        x_timeseries, x_order = inputs
+        i_half = x_timeseries.shape[1] // 2
+
+        split = x_timeseries[:, i_half:, ]
+        mean = torch.mean(split[:, :, 0]).item()
+
+        x_ens, x_ent = self.series_decomp(split)
+        x0 = torch.zeros([x_timeseries.shape[0], self.pred_len, x_timeseries.shape[2]])
+        x_mean = torch.full((x_timeseries.shape[0], self.pred_len, x_timeseries.shape[2]), mean)
+
+        x_decoder = torch.cat([x_ens, x0], dim=1)
+        x_mean = torch.cat([x_ent, x_mean], dim=1)
+
+        x_order = self.encoder(x_order)
+
+        x_decoder, x_mean = self.decoder(x_decoder, x_order, x_mean)
+
+        x_pred = (x_decoder + x_mean)[:, i_half:i_half + self.pred_len, :1]
+
+        return x_pred
+
+    def save_checkpoint(self, path, optimizer, epoch, loss):
+        logging.info(f'Saving checkpoint to {path}')
+        torch.save({
+            'epoch': epoch,
+            'model': self.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': loss
+        }, path)
+
+    @classmethod
+    def load_checkpoint(cls, model, optimizer, path):
+        checkpoint = torch.load(path)
+
+        model.load_state_dict(checkpoint['model'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+        loss = checkpoint['loss']
+        epoch = checkpoint['epoch']
+
+        return model, optimizer, loss, epoch
